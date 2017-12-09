4874b4f2f121932f433c55499926c726df12bd0a
AfryMask <AfryMask@163.com>
2017-12-09 18:31:19 +0800

    init

---
+++ AFAnimateDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AFAnimateDemo
+//
+//  Created by 初毅 on 2017/12/9.
+//  Copyright © 2017年 初毅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AFAnimateDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AFAnimateDemo
+//
+//  Created by 初毅 on 2017/12/9.
+//  Copyright © 2017年 初毅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
